39ab432b40eb71e48099669414ffe633a009b436
Sivasankar <sivasankar83.m@gmail.com>
2026-06-10 09:48:10 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.post('/grades', (req, res) => {
   res.json(newGrade);
 });
 
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
   console.log(`Grade service is running on port ${port}`);
 });
